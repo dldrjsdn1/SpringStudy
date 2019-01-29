91437a1b92efdab4bd693f2f85818ddbb4740ec0
dldrjsdn1 <dldrjsdn1@naver.com>
2019-01-29 22:56:13 +0900

    2019.01.29

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -1,6 +1,6 @@
 package com.springbook.biz.board.impl;
 
-import java.util.List; 
+import java.util.List;  
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 
+
 @Service("boardService")
 public class BoardServiceImpl  implements BoardService{
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
 
+	
 	public void insertBoard(BoardVO vo){
 		boardDAO.insertBoard(vo);
 	}
@@ -32,6 +34,4 @@ public class BoardServiceImpl  implements BoardService{
 	public List<BoardVO> getBoardList(BoardVO vo){
 		return boardDAO.getBoardList(vo);
 	}
-	
-	
 }
