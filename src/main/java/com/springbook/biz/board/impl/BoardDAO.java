2df6ccc81b428614af324de93f250dd97de8c73f
dldrjsdn1 <dldrjsdn1@naver.com>
2019-02-01 22:13:11 +0900

    2019.02.01

--- src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -1,6 +1,6 @@
 package com.springbook.biz.board.impl;
 
-import java.sql.Connection;  
+import java.sql.Connection;   
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -21,11 +21,11 @@ public class BoardDAO {
 	private ResultSet rs = null;
 	
 	//SQL 명령어들
-	private final String BOARD_INSERT = "insert into board(seq,title,writer,content) values((select nvl(max(seq),0)+1 from board),?,?,?)";
+	private final String BOARD_INSERT = "insert into board(seq,title,writer,content) values((select nvl(max(seq), 0)+1 from board),?,?,?)";
 	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
-	private final String BOARD_DELETE = "delete * from board where seq=?";
+	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
-	private final String BOARD_LIST = "select *from board order by seq desc";
+	private final String BOARD_LIST = "select * from board order by seq desc";
 	
 	//CRUD	 기능의 메소드 구현
 	//글 등록
@@ -37,6 +37,7 @@ public class BoardDAO {
 			stmt.setString(1, vo.getTitle());
 			stmt.setString (2, vo.getWriter());
 			stmt.setString (3, vo.getContent());
+			stmt.executeUpdate();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
