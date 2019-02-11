e321f9871b08afaaf998b152ca10671c2c29a9b7
dldrjsdn1 <dldrjsdn1@naver.com>
2019-02-11 23:13:51 +0900

    2019.02.11

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,6 +1,9 @@
 package com.springbook.view.board;
 
-import java.util.HashMap;   
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,20 +11,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.springbook.biz.board.BoardListVO;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-
-import java.io.File;
-import java.io.IOException;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @SessionAttributes("board")
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
+	
+	@RequestMapping("/dataTransform.do")
+	@ResponseBody
+	public BoardListVO dataTransform(BoardVO vo){
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		BoardListVO boardListVO = new BoardListVO();
+		boardListVO.setBoardList(boardList);
+		return boardListVO;
+	}
 	
 	//±Û µî·Ï
 	@RequestMapping(value="/insertBoard.do")
--- src/main/webapp/getBoardList.jsp
+++ src/main/webapp/getBoardList.jsp
@@ -1,6 +1,7 @@
 <%@ page  contentType="text/html; charset=UTF-8"%>
 <%@taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
 <%@taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
 <% request.setCharacterEncoding("euc-kr"); %>
 
@@ -44,14 +45,13 @@
 	<td bgcolor="orange" width="100"><spring:message code="message.board.list.table.head.cnt"/></td>
 </tr>
 
-<c:forEach items="${boardList }" var="board">
+<c:forEach items="${boardList}" var="board">
 <tr>
 	<td>${board.seq}</td>
-	<td align="left"><a href="getBoard.do?seq=${board.seq
-	}">${board.title}</a></td>
-	 <td>${board.writer}</td>
-	 <td>${board.regDate}</td>
-	 <td>${board.cnt}</td>
+	<td><a href="getBoard.do?seq=${board.seq}">${board.title}</a></td>
+	<td>${board.writer}</td>
+	<td><fmt:formatDate value="${board.regDate}" pattern="yyyy-MM--dd" /></td>
+	<td>${board.cnt}</td>
 </tr>
 </c:forEach>
 </table>
